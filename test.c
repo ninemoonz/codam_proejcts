98be0f137e521d7d9f01289aa2d71204d89dcff6
Koodal Kweon <kkweon@f0r3s10.codam.nl>
2025-11-24 17:05:14 +0100

    ft_printf start

---
+++ test.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 16:53:09 by kkweon            #+#    #+#             */
+/*   Updated: 2025/11/24 17:01:08 by kkweon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int i;
+
+	printf("hello world\n");
+	printf("hello world I'm %d years old %%\n", 22);
+	scanf("%d", &i);
+	printf("%d\n", i);
+	printf("%i\n", i);
+	printf("cspdiuxX%");
+	return (0);
+}
